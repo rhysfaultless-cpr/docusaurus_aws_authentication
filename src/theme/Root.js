278e84ff7bfeccaac98f99061570b64710f9f4e2
Rhys Faultless <rfaultless@clearpath.ai>
2022-11-24 15:43:04 -0500

    removed unused State variables and update function

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -7,21 +7,6 @@ import '@aws-amplify/ui-react/styles.css';
 import awsExports from '../aws-exports';
 Amplify.configure(awsExports);
 
-async function fetchTenant(setTenant, setUserIsCustomer, setUserIsReseller, setUserIsEmployee) {
-  // get the access token of the signed in user
-  const {accessToken} = await Auth.currentSession();
-  // get the tenant from the top of the cognito groups list
-  const cognitogroups = accessToken.payload['cognito:groups'];
-  const tenant = cognitogroups;
-  setTenant(tenant);
-  const tempUserIsCustomer = (tenant.indexOf('employee') !== -1) || (tenant.indexOf('reseller') !== -1) || (tenant.indexOf('customer') !== -1)
-  const tempUserIsReseller = (tenant.indexOf('employee') !== -1) || (tenant.indexOf('reseller') !== -1)
-  const tempUserIsEmployee = (tenant.indexOf('employee') !== -1)
-  setUserIsCustomer(tempUserIsCustomer);
-  setUserIsReseller(tempUserIsReseller);
-  setUserIsEmployee(tempUserIsEmployee);
-}
-
 function Root({children}) {
   return (
     <>
