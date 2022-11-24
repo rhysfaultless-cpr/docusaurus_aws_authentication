be5c8df610d8fcb900b20fa38c136b1c676c9bcc
Rhys Faultless <rfaultless@clearpath.ai>
2022-11-24 15:33:56 -0500

    Added Privileged page with, function to load content if User's login is included in AWS Cognito Group.

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
-import { Amplify } from 'aws-amplify';
+import { Amplify, Auth } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import awsExports from '../aws-exports';
 Amplify.configure(awsExports);
+
+async function fetchTenant(setTenant, setUserIsCustomer, setUserIsReseller, setUserIsEmployee) {
+  // get the access token of the signed in user
+  const {accessToken} = await Auth.currentSession();
+  // get the tenant from the top of the cognito groups list
+  const cognitogroups = accessToken.payload['cognito:groups'];
+  const tenant = cognitogroups;
+  setTenant(tenant);
+  const tempUserIsCustomer = (tenant.indexOf('employee') !== -1) || (tenant.indexOf('reseller') !== -1) || (tenant.indexOf('customer') !== -1)
+  const tempUserIsReseller = (tenant.indexOf('employee') !== -1) || (tenant.indexOf('reseller') !== -1)
+  const tempUserIsEmployee = (tenant.indexOf('employee') !== -1)
+  setUserIsCustomer(tempUserIsCustomer);
+  setUserIsReseller(tempUserIsReseller);
+  setUserIsEmployee(tempUserIsEmployee);
+}
 
 function Root({children}) {
   return (
